d85b5cf925435c34da5b9c4738ce81cd6a60c074
Jonathan Suh <hello@jonsuh.com>
2016-02-23 22:13:30 -0600

    JS for toggling is-active class.

--- assets/_js/hamburgers.js
+++ assets/_js/hamburgers.js
@@ -1,9 +1,49 @@
-var Hameburgers = Hameburgers || {};
+(function() {
+  "use strict";
 
-Hameburgers = {
-  make: function() {
+  /**
+   * forEach implementation for Objects/NodeLists/Arrays, automatic type loops and context options
+   *
+   * @private
+   * @author Todd Motto
+   * @link https://github.com/toddmotto/foreach
+   * @param {Array|Object|NodeList} collection - Collection of items to iterate, could be an Array, Object or NodeList
+   * @callback requestCallback      callback   - Callback function for each iteration.
+   * @param {Array|Object|NodeList} scope=null - Object/NodeList/Array that forEach is iterating over, to use as the this value when executing callback.
+   * @returns {}
+   */
+  var forEach = function (collection, callback, scope) {
+    if (Object.prototype.toString.call(collection) === "[object Object]") {
+      for (var prop in collection) {
+        if (Object.prototype.hasOwnProperty.call(collection, prop)) {
+          callback.call(scope, collection[prop], prop, collection);
+        }
+      }
+    } else {
+      for (var i = 0, len = collection.length; i < len; i++) {
+        callback.call(scope, collection[i], i, collection);
+      }
+    }
+  };
 
+  var Hamburgers = {
+    make: function() {
+      var hamburgers = document.querySelectorAll(".js-hamburger");
+
+      if (hamburgers.length > 0) {
+        forEach(hamburgers, function(hamburger) {
+          hamburger.addEventListener("click", Hamburgers.clickHandle, false);
+        });
+      }
+    },
+
+    clickHandle: function(e) {
+      e.preventDefault();
+
+      this.classList.toggle("is-active");
+    }
   }
-}
 
-Hameburgers.make();
+  Hamburgers.make();
+
+}());
